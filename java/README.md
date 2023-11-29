316b0ace28f1c264dbe4c727a8b09c583911d316
M0N0G0N <thibauddandoy3d@gmail.com>
2023-11-29 10:55:19 +0100

    Without Indexing Co-authored-by: Senhua-Liu <Senhua-Liu@users.noreply.github.com> Co-authored-by: Malo Le Corvec <malo.le.corvec@efrei.net>

--- java/README.md
+++
@@ -1,8 +0,0 @@
-# Java
-
-Run the application with:
-
-```
-mvn compile
-mnv exec:java -Dexec.args=<Number of contacts>
-```
--- javascript/index.js
+++ javascript/index.js
@@ -13,33 +13,40 @@ const shouldMigrate = !fs.existsSync(filename)
  * one at a time
  *
  */
-function * generateContacts () {
- // TODO
-  yield [`name-1`, `email-1@domain.tld`]
-  yield [`name-2`, `email-2@domain.tld`]
-  yield [`name-3`, `email-3@domain.tld`]
+function* generateContacts() {
+  for (let index = 1; index <= numContacts; index++) {
+    yield [`name-${index}`, `email-${index}@domain.tld`]
+  }
 }
 
 const migrate = async (db) => {
   console.log('Migrating db ...')
+  await db.run(`DROP TABLE IF EXISTS contacts`)
   await db.exec(`
         CREATE TABLE contacts(
           id INTEGER PRIMARY KEY,
           name TEXT NOT NULL,
           email TEXT NOT NULL
-         )
-     `)
+        )
+    `)
   console.log('Done migrating db')
 }
 
 const insertContacts = async (db) => {
   console.log('Inserting contacts ...')
   // TODO
+  const contactsGenerator = generateContacts(numContacts);
+  const placeholder = [...Array(numContacts)].map(() => '(?, ?)').join(', ');
+  const contacts = [...contactsGenerator];
+  await db.run(`INSERT INTO contacts(name, email) VALUES ${contacts.map((contact)=> `("${contact[0]}", "${contact[1]}")`)}` )
+  console.log('Done inserting contacts')
 }
+
 
 const queryContact = async (db) => {
   const start = Date.now()
   const res = await db.get('SELECT name FROM contacts WHERE email = ?', [`email-${numContacts}@domain.tld`])
+  console.log(res)
   if (!res || !res.name) {
     console.error('Contact not found')
     process.exit(1)
@@ -54,9 +61,7 @@ const queryContact = async (db) => {
     filename,
     driver: sqlite3.Database
   })
-  if (shouldMigrate) {
-    await migrate(db)
-  }
+  await migrate(db)
   await insertContacts(db)
   await queryContact(db)
   await db.close()
